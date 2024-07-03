353205dee20c6d2df6f2b115bb14a027864e7295
ans <ans@nts.ch>
2024-07-03 17:30:28 +0200

    added some comments

--- InvoiceGenerator/InvoiceDetails.cs
+++ InvoiceGenerator/InvoiceDetails.cs
@@ -2,25 +2,28 @@ using System.Globalization;
 
 public class InvoiceDetails
 {
-    public string Date { get; }
-    public string FirstDateMonth { get; }
-    public float HourlyWage { get; }
-    public float Hours { get; }
-    public string LastDateMonth { get; }
-    public float MWSTRate { get; }
-    public float MWSTPrice { get; }
-    public string MonthYear { get; }
-    public string Place { get; }
-    public string Recipient { get; }
-    public string Street { get; }
-    public float TotalPrice { get; }
-    public float TotalPriceInclMWST { get; }
-    public string ZIP { get; }
-
+    // Public properties representing various details of an invoice
+    public string Date { get; }               // The date of the invoice
+    public string FirstDateMonth { get; }     // The first date of the billing month
+    public float HourlyWage { get; }          // The hourly wage
+    public float Hours { get; }               // The number of hours worked
+    public string LastDateMonth { get; }      // The last date of the billing month
+    public float MWSTRate { get; }            // The MWST rate
+    public float MWSTPrice { get; }           // The MWST amount
+    public string MonthYear { get; }          // The billing month and year
+    public string Place { get; }              // The place of the recipient
+    public string Recipient { get; }          // The recipient of the invoice
+    public string Street { get; }             // The street address of the recipient
+    public float TotalPrice { get; }          // The total price before MWST
+    public float TotalPriceInclMWST { get; }  // The total price including MWST
+    public string ZIP { get; }                // The ZIP code of the recipient
 
     public InvoiceDetails()
     {
+        // Set the current date as the invoice date
         Date = DateTime.Now.ToString("dd.MM.yyyy");
+
+        // Set a fixed hourly wage and MWST rate
         HourlyWage = 150;
         MWSTRate = 8.1f;
 
@@ -28,24 +31,32 @@ public class InvoiceDetails
         DateTime currentDate = DateTime.Now;
         if (currentDate.Day <= 15)
         {
+            // If the current day is on or before the 15th, use the previous month for billing
             FirstDateMonth = new DateTime(currentDate.Year, currentDate.Month - 1, 1).ToString("dd.MM.yyyy");
             LastDateMonth = new DateTime(currentDate.Year, currentDate.Month - 1, DateTime.DaysInMonth(currentDate.Year, currentDate.Month - 1)).ToString("dd.MM.yyyy");
             MonthYear = new DateTime(currentDate.Year, currentDate.Month - 1, 1).ToString("MMMM yyyy", CultureInfo.CreateSpecificCulture("de-DE"));
         }
         else
         {
+            // If the current day is after the 15th, use the current month for billing
             FirstDateMonth = new DateTime(currentDate.Year, currentDate.Month, 1).ToString("dd.MM.yyyy");
             LastDateMonth = new DateTime(currentDate.Year, currentDate.Month, DateTime.DaysInMonth(currentDate.Year, currentDate.Month)).ToString("dd.MM.yyyy");
             MonthYear = new DateTime(currentDate.Year, currentDate.Month, 1).ToString("MMMM yyyy", CultureInfo.CreateSpecificCulture("de-DE"));
         }
 
+        // Prompt the user to input the hours worked
         Console.Write("Hours worked: ");
         string? hoursInput = Console.ReadLine();
-        Hours = float.TryParse(hoursInput, out float h) ? h : 0f; // Default to 0 if parsing fails
 
+        // Parse the input hours, default to 0 if parsing fails
+        Hours = float.TryParse(hoursInput, out float h) ? h : 0f;
+
+        // Set fixed values for place, recipient, street, and ZIP code
         Place = "Test";
         Recipient = "Test Customer AG";
         Street = "Test Street";
+
+        // Calculate total prices based on hours worked and hourly wage
         TotalPrice = RoundToNearest5Rappen(HourlyWage * Hours);
         MWSTPrice = TotalPrice * (MWSTRate / 100);
         TotalPriceInclMWST = RoundToNearest5Rappen(TotalPrice + MWSTPrice);
